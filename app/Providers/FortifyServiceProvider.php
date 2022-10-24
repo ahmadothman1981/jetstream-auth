0385fe89075b0867b658b841dfea487ef7db5df4
ahmadothman1981 <ahmadothman@outlook.com>
2022-10-24 10:35:00 +0200

    setup multi auth admin

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -11,6 +11,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use App\Actions\Fortify\AttemptToAuthenticate;
+use App\Actions\Fortify\RedirectIfTwoFactorAuthenticatable;
+use App\Http\Controllers\AdminController;
+use Auth;
+
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -21,7 +27,9 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->when([AdminController::class,AttemptToAuthenticate::class,RedirectIfTwoFactorAuthenticatable::class])->needs(StatefulGuard::class)->give(function(){
+            retutn Auth::guard('admin');
+        });
     }
 
     /**
