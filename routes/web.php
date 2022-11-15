f75aa8dad27e7d35343845551fe215a72414dea4
ahmadothman1981 <ahmadothman@outlook.com>
2022-11-15 18:59:23 +0200

    creating product table

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Frontend\IndexController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\SubCategoryController;
+use App\Http\Controllers\Backend\ProductController;
 use App\Models\User;
 
 /*
@@ -114,6 +115,16 @@ Route::prefix('category')->group(function(){
        Route::get('/sub/sub/edit/{id}',[SubCategoryController::class,'SubSubCategoryEdit'])->name('subsubcategory.edit');
        Route::post('/sub/sub/update',[SubCategoryController::class,'SubSubCategoryUpdate'])->name('subsubcategory.update');
         Route::get('/sub/sub/delete/{id}',[SubCategoryController::class,'SubSubCategoryDelete'])->name('subsubcategory.delete');
+
+
+});
+
+/////////////////////////////////////////////////////////////////
+///////////////////////////ALL Products Routes//////////////////
+
+Route::prefix('product')->group(function(){
+     Route::get('/add',[ProductController::class,'AddProduct'])->name('add-product');
+     
 
 
 });
