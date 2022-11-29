86cbc93fae6bf4c9f3717acee9593f7aa00f7176
ahmadothman1981 <ahmadothman@outlook.com>
2022-11-29 16:03:44 +0200

    Frontend Product Discount ,Product Price Show With Multiple Language

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -998,16 +998,41 @@
                         <div class="product-image">
                           <div class="image"> <a href="detail.html"><img  src="{{ asset($product->product_thambnail) }}" alt=""></a> </div>
                           <!-- /.image -->
-                          
-                          <div class="tag new"><span>new</span></div>
+  @php
+            $amount = $product->selling_price - $product->discount_price;
+            $discount = ($amount/$product->selling_price) * 100;
+  @endphp
+
+              <div>
+                @if($product->discount_price== NULL)
+                 <div class="tag new"><span>new</span></div>
+                 @else
+                 <div class="tag hot"><span>{{round($discount)}}%</span></div>
+                  @endif
+              </div>
+                        
                         </div>
                         <!-- /.product-image -->
                         
                         <div class="product-info text-left">
-                          <h3 class="name"><a href="detail.html">{{ $product->product_name_en }}</a></h3>
+                          <h3 class="name"><a href="detail.html">
+@if(session()->get('Language') == 'Arabic' )   {{ $product->product_name_ar }}  
+@else  {{ $product->product_name_en }} 
+@endif
+
+                           </a></h3>
                           <div class="rating rateit-small"></div>
                           <div class="description"></div>
-                          <div class="product-price"> <span class="price"> {{ $product->selling_price }} </span> <span class="price-before-discount"> {{$product->discount_price }}</span> </div>
+ @if($product->discount_price== NULL)
+       <div class="product-price">
+         <span class="price"> {{ $product->selling_price }} </span>
+         
+        </div>
+ @else
+ <div class="product-price"> <span class="price"> {{ $product->discount_price}} </span> <span class="price-before-discount"> {{$product->selling_price }}</span> </div>
+ @endif
+
+                          
                           <!-- /.product-price --> 
                           
                         </div>
@@ -1062,16 +1087,41 @@
                         <div class="product-image">
                           <div class="image"> <a href="detail.html"><img  src="{{ asset($product->product_thambnail) }}" alt=""></a> </div>
                           <!-- /.image -->
-                          
-                          <div class="tag new"><span>new</span></div>
+  @php
+            $amount = $product->selling_price - $product->discount_price;
+            $discount = ($amount/$product->selling_price) * 100;
+  @endphp
+
+              <div>
+                @if($product->discount_price== NULL)
+                 <div class="tag new"><span>new</span></div>
+                 @else
+                 <div class="tag hot"><span>{{round($discount)}}%</span></div>
+                  @endif
+              </div>
+                        
                         </div>
                         <!-- /.product-image -->
                         
                         <div class="product-info text-left">
-                          <h3 class="name"><a href="detail.html">{{ $product->product_name_en }}</a></h3>
+                          <h3 class="name"><a href="detail.html">
+@if(session()->get('Language') == 'Arabic' )   {{ $product->product_name_ar }}  
+@else  {{ $product->product_name_en }} 
+@endif
+
+                           </a></h3>
                           <div class="rating rateit-small"></div>
                           <div class="description"></div>
-                          <div class="product-price"> <span class="price"> {{ $product->selling_price }} </span> <span class="price-before-discount"> {{$product->discount_price }}</span> </div>
+ @if($product->discount_price== NULL)
+       <div class="product-price">
+         <span class="price"> {{ $product->selling_price }} </span>
+         
+        </div>
+ @else
+ <div class="product-price"> <span class="price"> {{ $product->discount_price}} </span> <span class="price-before-discount"> {{$product->selling_price }}</span> </div>
+ @endif
+
+                          
                           <!-- /.product-price --> 
                           
                         </div>
