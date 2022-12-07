c24618ec792924f906c18dbb9623fdc9c21cab75
ahmadothman1981 <ahmadothman@outlook.com>
2022-12-07 14:41:25 +0200

    design add cart modal

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -88,3 +88,71 @@
  @endif 
 </script>
 
+<!-- Add to Cart Product  Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Product Name</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       <div class="row">
+         <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..." style="height: 200px; width: 200px;">
+  
+            </div>
+         </div><!-- END  col-md -->
+
+         <div class="col-md-4">
+            <ul class="list-group">
+  <li class="list-group-item">Product Price:</li>
+  <li class="list-group-item">Product Code:</li>
+  <li class="list-group-item">Product Category:</li>
+  <li class="list-group-item">Product Brand:</li>
+  <li class="list-group-item">Product Stock:</li>
+            </ul>
+         </div><!-- END  col-md -->
+
+         <div class="col-md-4">
+                 <div class="form-group">
+    <label for="exampleFormControlSelect1">Choose Color</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+                  </div><!-- END  Form Group -->
+                  <div class="form-group">
+    <label for="exampleFormControlSelect1">Choose Size</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+                  </div><!-- END  Form Group -->
+
+       <div class="form-group">
+    <label for="exampleFormControlInput1">Quantity</label>
+    <input type="number" class="form-control" id="exampleFormControlInput1" value="1" min="1">
+      </div><!-- END  Form Group -->
+  <button type="submit" class="btn btn-primary mb-2">Add To Cart</button>
+         </div><!-- END  col-md -->
+
+       </div><!-- END  row -->
+      </div><!-- END  Modal Body -->
+      
+    </div>
+  </div>
+</div>
+
+<!-- END Cart Product  Modal -->
+</body>
+</html>
